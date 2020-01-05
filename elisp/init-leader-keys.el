35d3a4a23e00551da679efa646d18c76ec176a58
alex <Alex de Wit>
2020-01-04 21:43:44 -0500

    add large template for my-leader-map

--- elisp/init-leader-keys.el
+++ elisp/init-leader-keys.el
@@ -31,6 +31,81 @@
 
 ;;; Code:
 
+(defvar my-leader-map nil "The keymap for my leader keybindings.")
+
+;; template for the leader key definitions
+((setq my-leader-map (make-sparse-keymap))
+       (evil-define-key 'normal my-leader-map "a" 'x)
+       (evil-define-key 'normal my-leader-map "b" 'x)
+       (evil-define-key 'normal my-leader-map "c" 'x)
+       (evil-define-key 'normal my-leader-map "d" 'x)
+       (evil-define-key 'normal my-leader-map "e" 'x)
+       (evil-define-key 'normal my-leader-map "f" 'x)
+       (evil-define-key 'normal my-leader-map "g" 'x)
+       (evil-define-key 'normal my-leader-map "h" 'x)
+       (evil-define-key 'normal my-leader-map "i" 'x)
+       (evil-define-key 'normal my-leader-map "j" 'x)
+       (evil-define-key 'normal my-leader-map "k" 'x)
+       (evil-define-key 'normal my-leader-map "l" 'x)
+       (evil-define-key 'normal my-leader-map "m" 'x)
+       (evil-define-key 'normal my-leader-map "n" 'x)
+       (evil-define-key 'normal my-leader-map "o" 'x)
+       (evil-define-key 'normal my-leader-map "p" 'x)
+       (evil-define-key 'normal my-leader-map "q" 'x)
+       (evil-define-key 'normal my-leader-map "r" 'x)
+       (evil-define-key 'normal my-leader-map "s" 'x)
+       (evil-define-key 'normal my-leader-map "t" 'x)
+       (evil-define-key 'normal my-leader-map "u" 'x)
+       (evil-define-key 'normal my-leader-map "v" 'x)
+       (evil-define-key 'normal my-leader-map "w" 'x)
+       (evil-define-key 'normal my-leader-map "x" 'x)
+       (evil-define-key 'normal my-leader-map "y" 'x)
+       (evil-define-key 'normal my-leader-map "z" 'x)
+       (evil-define-key 'normal my-leader-map "[" 'x)
+       (evil-define-key 'normal my-leader-map "]" 'x)
+       (evil-define-key 'normal my-leader-map "\\" 'x)
+       (evil-define-key 'normal my-leader-map ";" 'x)
+       (evil-define-key 'normal my-leader-map "'" 'x)
+       (evil-define-key 'normal my-leader-map "," 'x)
+       (evil-define-key 'normal my-leader-map "." 'x)
+       (evil-define-key 'normal my-leader-map "/" 'x)
+
+       (evil-define-key 'normal my-leader-map "A" 'x)
+       (evil-define-key 'normal my-leader-map "B" 'x)
+       (evil-define-key 'normal my-leader-map "C" 'x)
+       (evil-define-key 'normal my-leader-map "D" 'x)
+       (evil-define-key 'normal my-leader-map "E" 'x)
+       (evil-define-key 'normal my-leader-map "F" 'x)
+       (evil-define-key 'normal my-leader-map "G" 'x)
+       (evil-define-key 'normal my-leader-map "H" 'x)
+       (evil-define-key 'normal my-leader-map "I" 'x)
+       (evil-define-key 'normal my-leader-map "J" 'x)
+       (evil-define-key 'normal my-leader-map "K" 'x)
+       (evil-define-key 'normal my-leader-map "L" 'x)
+       (evil-define-key 'normal my-leader-map "M" 'x)
+       (evil-define-key 'normal my-leader-map "N" 'x)
+       (evil-define-key 'normal my-leader-map "O" 'x)
+       (evil-define-key 'normal my-leader-map "P" 'x)
+       (evil-define-key 'normal my-leader-map "Q" 'x)
+       (evil-define-key 'normal my-leader-map "R" 'x)
+       (evil-define-key 'normal my-leader-map "S" 'x)
+       (evil-define-key 'normal my-leader-map "T" 'x)
+       (evil-define-key 'normal my-leader-map "U" 'x)
+       (evil-define-key 'normal my-leader-map "V" 'x)
+       (evil-define-key 'normal my-leader-map "W" 'x)
+       (evil-define-key 'normal my-leader-map "X" 'x)
+       (evil-define-key 'normal my-leader-map "Y" 'x)
+       (evil-define-key 'normal my-leader-map "Z" 'x)
+       (evil-define-key 'normal my-leader-map "{" 'x)
+       (evil-define-key 'normal my-leader-map "}" 'x)
+       (evil-define-key 'normal my-leader-map "|" 'x)
+       (evil-define-key 'normal my-leader-map ":" 'x)
+       (evil-define-key 'normal my-leader-map "\"" 'x)
+       (evil-define-key 'normal my-leader-map "<" 'x)
+       (evil-define-key 'normal my-leader-map ">" 'x)
+       (evil-define-key 'normal my-leader-map "?" 'x))
+
+;; define use-package keyword :leader
 (with-eval-after-load 'use-package-core
   (declare-function use-package-concat "use-package")
   (declare-function use-package-process-keywords "use-package")
@@ -48,6 +123,29 @@
                  unless (eq item :leader)
                  collect item))
 
+(defun use-package-normalize/:general (_name _keyword general-arglists)
+    "Return a plist containing the original ARGLISTS and autoloadable symbols."
+    (let* ((sanitized-arglist
+            ;; combine arglists into one without function names or
+            ;; positional arguments
+            (cl-loop for arglist in general-arglists
+                     append (general--sanitize-arglist arglist)))
+           (commands
+            (cl-loop for (key def) on sanitized-arglist by 'cddr
+                     when (and (not (keywordp key))
+                               (not (null def))
+                               (ignore-errors
+                                 ;; remove extra quote
+                                 ;; `eval' works in some cases that `cadr' does
+                                 ;; not (e.g. quoted string, '(list ...), etc.)
+                                 ;; `ignore-errors' handles cases where it fails
+                                 ;; (e.g. variable not defined at
+                                 ;; macro-expansion time)
+                                 (setq def (eval def))
+                                 (setq def (general--extract-autoloadable-symbol
+                                            def))))
+                     collect def)))
+      (list :arglists general-arglists :commands commands)))
 
   (defun use-package-autoloads/:general (_name _keyword args)
     "Return an alist of commands extracted from ARGS.
