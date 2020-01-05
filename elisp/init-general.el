6bb4264c69046091fb7a101bc8f2c7e8694f4122
alex <alex@nafnlaus.ca>
2020-01-04 23:06:55 -0500

    add general.el

---
+++ elisp/init-general.el
@@ -0,0 +1,37 @@
+;;; init-general.el ---   -*- lexical-binding: t -*-
+
+;; Author: Alex de Wit
+;; Maintainer: Alex de Wit
+;; Version: 0
+;; Package-Requires: ()
+;; Homepage: none
+;; Keywords: general, keybindings
+
+
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; This file initializes general.el for my Emacs 
+
+;;; Code:
+
+(use-package general)
+
+(provide 'init-general)
+
+;;; init-general.el ends here
